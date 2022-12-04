d663eb5581675d8a874c948246ccfb6d7ebf99a8
solenerichard69 <solene.richard1@gmail.com>
2022-12-04 23:17:49 +0100

    first commit

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+<!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-BKV9CF4L6E"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-BKV9CF4L6E');
+</script>
+"""
+ return prefix_google + "Hello World"
